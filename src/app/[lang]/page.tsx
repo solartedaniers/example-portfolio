d5f97c7d06ae0b94efb5a1aa3e79e897deb2964b
Daniers <solartedaniers@gmail.com>
2025-10-15 09:59:10 -0500

    Subiendo a vercel

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { getDictionary } from "../i18n/dictionaries";
 import { Lang } from "../i18n/config";
 import LanguageSwitcher from "@/components/LanguageSwitcher";
@@ -15,19 +14,19 @@ import DetailsSection from "@/components/DetailsSection";
 
 import ThemeToggle from "@/components/ThemeToggle";
 
-export default async function HomePage(props: {
-  params: Promise<{ lang: Lang }>;
-}) {
+export default async function HomePage(props: { params: Promise<{ lang: Lang }> }) {
   const { lang } = await props.params; 
   const dict = await getDictionary(lang);
 
   return (
     <div className="font-sans flex justify-center transition-colors duration-300 bg-background text-foreground min-h-screen p-4">
+      {/* Language & Theme Toggle */}
       <div className="fixed top-4 right-4 flex gap-2 z-50">
         <LanguageSwitcher currentLang={lang} />
         <ThemeToggle />
       </div>
 
+      {/* Main Grid */}
       <main className="grid grid-cols-12 container mt-20 gap-10 sm:grid-cols-6 md:grid-cols-8 lg:grid-cols-12">
         <AvatarSection />
         <IntroSection dict={dict} />
@@ -39,8 +38,7 @@ export default async function HomePage(props: {
         <EditingTools dict={dict} />
         <LanguagesSection dict={dict} />
         <PortfolioSection />
-        <DetailsSection />
-
+        <DetailsSection /> 
       </main>
     </div>
   );
