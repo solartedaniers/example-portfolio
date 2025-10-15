440bb25c3b3efd3d21594b18eb9142b6e5467a1e
Jhonatan Mideros <10jonmid@gmail.com>
2025-10-14 19:07:43 -0500

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,39 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
+    <div className='font-sans flex justify-center'>
+      <main className='grid grid-cols-12 container mt-20 gap-10'>
+        {/* <Image src='/next.svg' alt='avatar' width={100} height={100} /> */}
+        <span className='col-span-3 row-span-2 bg-white rounded-2xl'>IMG</span>
+        <p className='p-5 text-2xl text-white col-span-9 rounded-2xl bg-foreground'>
+          My name is Sunil Kumar self- taught logo/brand designer with 4+ years
+          of experience creating modern, clean, and minimal brands that make a
+          lasting impression.
+        </p>
+        <ul className='flex items-center col-span-9 gap-4 p-5 rounded-2xl bg-foreground'>
+          <li className='text-white text-lg font-bold border-r pr-4'>
+            Interests
           </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+          <li className='flex items-center bg-background rounded-lg text-white text-sm px-4 py-2'>
+            🎮 Gaming
           </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <li className='flex items-center bg-background rounded-lg text-white text-sm px-4 py-2'>
+            📽️ Film Making
+          </li>
+          <li className='flex items-center bg-background rounded-lg text-white text-sm px-4 py-2'>
+            ✈️ Traveling
+          </li>
+        </ul>
+        <div className='col-span-6 rounded-2xl bg-foreground p-5'>
+          <h3 className='text-2xl font-bold text-white'>Freelancer</h3>
+          <p className='text-white'>Logo/Brand Designer</p>
+        </div>
+        <div className='col-span-6 rounded-2xl bg-foreground p-5'>
+          <h3 className='text-2xl font-bold text-white'>Meetzed</h3>
+          <p className='text-white'>Logo/Brand Designer</p>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
